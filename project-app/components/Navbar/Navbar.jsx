8ec9550e0cdba5c8d214bf11019ec2a4ac41fb4c
Amira <amiragucher@gmail.com>
2022-07-28 10:11:12 +0100

    Basic nav-bar added

---
+++ project-app/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+// import SocialMedia from "../SocialMedia/SocialMedia"
+
+export default function Navbar () {
+    
+    return (
+        <div>
+            <a href="#home">Home</a>
+            <a href="#dropdown">Do Your Part</a>
+            <a href="#weather">Weather</a>
+            <a href="#contact">Contact Us</a>
+
+        </div>
+    )
+}
--- project-app/pages/index.js
+++ project-app/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar/Navbar";
 
 import dynamic from "next/dynamic";
 
@@ -12,6 +13,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className="text-blue-600/100 hover:underline">Project Clean-Up</h1>
+      <Navbar/>
       <MapWithNoSSR />
     </div>
   );
