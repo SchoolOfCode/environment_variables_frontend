402c918bec16f7e3878e94748934632e936cbbe9
Simran Rai <simran_rai982@outlook.com>
2022-07-28 15:38:05 +0100

    sidebar finished

--- project-app/components/Navbar/Navbar.jsx
+++ project-app/components/Navbar/Navbar.jsx
@@ -4,107 +4,130 @@ import { Dropdown, Menu, Space } from "antd";
 import "antd/dist/antd.css";
 import Link from "next/link";
 import { AiOutlineMenu } from "react-icons/ai";
-import { Button, Drawer } from 'antd';
-import React, { useState } from 'react';
+import { Button, Drawer } from "antd";
+import React, { useState } from "react";
 
 const Navbar = () => {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const showDrawer = () => {
-        setVisible(true);
-    };
+  const showDrawer = () => {
+    setVisible(true);
+  };
 
-    const onClose = () => {
-        setVisible(false);
-    };
+  const onClose = () => {
+    setVisible(false);
+  };
 
+  const menu = (
+    <Menu
+      items={[
+        {
+          key: "1",
+          label: (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.antgroup.com"
+            >
+              About Us
+            </a>
+          ),
+        },
+        {
+          key: "2",
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="#startclean">
+              Start A Cleanup
+            </a>
+          ),
+        },
+        {
+          key: "3",
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="#map">
+              Join A Cleanup
+            </a>
+          ),
+        },
+      ]}
+    />
+  );
 
-
-    const menu = (
-        <Menu
-            items={[
-                {
-                    key: "1",
-                    label: (
-                        <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href="https://www.antgroup.com"
-                        >
-                            About Us
-                        </a>
-                    ),
-                },
-                {
-                    key: "2",
-                    label: (
-                        <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href="https://www.aliyun.com"
-                        >
-                            Start A Cleanup
-                        </a>
-                    ),
-                },
-                {
-                    key: "3",
-                    label: (
-                        <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href="https://www.luohanacademy.com"
-                        >
-                            Join A Cleanup
-                        </a>
-                    ),
-                },
-            ]}
+  return (
+    <div className="fixed">
+      <div className=" flex justify-between w-[100vw] bg-[#004F54] text-[white] ml-[0px] px-[2em] py-[2em]">
+        <div>Logo</div>
+        <Link href="#home">
+          <a className="text-[white] hidden md:flex items-center ">Home</a>
+        </Link>
+        <Dropdown
+          overlay={menu}
+          className="text-[white] hidden md:flex items-center"
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              Do Your Part
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>
+        <Link href="#weather">
+          <a className="text-[white] hidden md:flex items-center ">Weather</a>
+        </Link>
+        <Link href="#contact">
+          <a className="text-[white] hidden md:flex items-center ">Contact</a>
+        </Link>
+        <AiOutlineMenu
+          className="md:hidden absolute inset-y-[2em] right-[2em]"
+          size={25}
+          onClick={showDrawer}
         />
-    );
-
-    return (
-        <div className="fixed">
-            <div className=" flex justify-between w-[100vw] bg-[#004F54] text-[white] ml-[0px] px-[2em] py-[2em]">
-                <div>Logo</div>
-                <Link href="#home">
-                    <a className="text-[white] hidden md:flex items-center ">Home</a>
-                </Link>
-                <Dropdown
-                    overlay={menu}
-                    className="text-[white] hidden md:flex items-center"
-                >
-                    <a onClick={(e) => e.preventDefault()}>
-                        <Space>
-                            Do Your Part
-                            <DownOutlined />
-                        </Space>
-                    </a>
-                </Dropdown>
-                <Link href="#weather">
-                    <a className="text-[white] hidden md:flex items-center ">Weather</a>
-                </Link>
-                <Link href="#contact">
-                    <a className="text-[white] hidden md:flex items-center ">Contact</a>
-                </Link>
-                <AiOutlineMenu
-                    className="md:hidden absolute inset-y-[2em] right-[2em]"
-                    size={25}
-                />
+      </div>
+      <>
+        <Drawer
+          title="Information"
+          placement="right"
+          onClose={onClose}
+          visible={visible}
+          className="text-[#004F54]"
+        >
+          <ul className="flex flex-col">
+            <div className="p-4">
+              <a className="text-[#004F54] text-xl" href="#home">
+                Home
+              </a>
             </div>
-            <>
-                <Button type="primary" onClick={showDrawer}>
-                    Open
-                </Button>
-                <Drawer title="Basic Drawer" placement="right" onClose={onClose} visible={visible} >
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                </Drawer>
-            </>
-        </div>
-    );
-}
+            <div className="p-4">
+              <a className="text-[#004F54] text-xl" href="about">
+                About
+              </a>
+            </div>
+            <div className="p-4">
+              <a className="text-[#004F54] text-xl" href="#map">
+                Join A Cleanup
+              </a>
+            </div>
+            <div className="p-4">
+              <a className="text-[#004F54] text-xl" href="#startcleanup">
+                Start A Cleanup
+              </a>
+            </div>
+            <div className="p-4">
+              <a className="text-[#004F54] text-xl" href="#weather">
+                Weather
+              </a>
+            </div>
+            <div className="p-4">
+              <a className="text-[#004F54] text-xl" href="#contact">
+                Contact
+              </a>
+            </div>
+          </ul>
+        </Drawer>
+      </>
+    </div>
+  );
+};
 
 export default Navbar;
 //SideBar Menu Plan
