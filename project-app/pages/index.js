7fb3879618135f3c3793a84821936e1a91c8f9d0
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-07-28 16:21:20 +0100

    tested all branches and merged to main, final cleanup of imports for skeleton

--- project-app/pages/index.js
+++ project-app/pages/index.js
@@ -2,28 +2,26 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar/Navbar";
-
 import dynamic from "next/dynamic";
 import LandingBanner from "../components/Landing/Landing";
+import Weather from "../components/Weather/Weather";
 
-// const MapWithNoSSR = dynamic(() => import("../components/Map/Map.jsx"), {
-//   ssr: false,
-// });
+const MapWithNoSSR = dynamic(() => import("../components/Map/Map.jsx"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Project Clean-Up|Home</title>
-    </Head>
-    <div className={styles.container}>
-      <Navbar />
-      <h1 className="text-blue-600/100 hover:underline">Project Clean-Up</h1>
-      <LandingBanner></LandingBanner>
-      
-      <MapWithNoSSR />
-      
-    </div>
+      <Head>
+        <title>Project Clean-Up|Home</title>
+      </Head>
+      <div className={styles.container}>
+        <Navbar />
+        <LandingBanner />
+        <MapWithNoSSR />
+        <Weather />
+      </div>
     </>
   );
 }
