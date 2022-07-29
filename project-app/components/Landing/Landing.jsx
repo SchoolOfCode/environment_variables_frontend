8f9d88aafa85593e3e3fb75399d1b1049adaf329
Emma <e.e.gill@outlook.com>
2022-07-29 09:43:05 +0100

    Continuing work on landing banner

--- project-app/components/Landing/Landing.jsx
+++ project-app/components/Landing/Landing.jsx
@@ -3,17 +3,20 @@ import NavButton from './NavButton/NavButton.jsx'
 const LandingBanner = () => {
 
   return ( 
-    <div className='bg-hero-background bg-contain bg-no-repeat .flex flex-col '>
+    <div id="home" className='bg-hero-background min-h-screen hero-image bg-right-bottom bg-cover flex'>
 
-      <div className='grid h-screen place-items-center'>
-      <h1 className = 'text-3xl'>Project Clean-Up</h1>
-      </div>
-
-      <div className='md:w-80 md:space-x-5 p-3 inset-9 md:-mt-96'>
-        <NavButton buttonText="Button One"/> 
-        <NavButton buttonText="Button Two"/>
+    <div class="relative container mx-auto p-4 flex items-center justify-center z-10">
+      <div>
+        <div class="flex flex-col justify-center items-center lg:items-start text-center">
+          <h1 class="text-6xl font-semibold text-gray-900 leading-none align-center">Project Clean-Up</h1>
+          <div class="flex flex-row items-center justify-between">
+            <NavButton buttonText="Button One"/> 
+            <NavButton buttonText="Button Two"/>
+          </div>
+        </div>
       </div>
     </div> 
+  </div> 
     );
 
 }
