68f75e345eb3432033b751f2a223c8f0675cab71
Jenan <j.azzubaydi@gmail.com>
2022-07-29 10:46:57 +0100

    landing-banner

--- project-app/components/Landing/Landing.jsx
+++ project-app/components/Landing/Landing.jsx
@@ -3,13 +3,13 @@ import NavButton from './NavButton/NavButton.jsx'
 const LandingBanner = () => {
 
   return ( 
-    <div id="home" className='bg-hero-background min-h-screen hero-image bg-right-bottom bg-cover flex'>
+    <div id="home" className='bg-hero-background min-h-screen hero-image bg-right-bottom bg-cover flex mt-[4rem]'>
 
-    <div class="relative container mx-auto p-4 flex items-center justify-center z-10">
-      <div>
+    <div class="relative container mx-auto p-4 flex items-center justify-center z-10" >
+      <div > 
         <div class="flex flex-col justify-center items-center lg:items-start text-center">
           <h1 class="text-6xl font-semibold text-gray-900 leading-none align-center">Project Clean-Up</h1>
-          <div class="flex flex-row items-center justify-between">
+          <div class="flex flex-row items-center justify-between space-x-8 md:ml-24">
             <NavButton buttonText="Button One"/> 
             <NavButton buttonText="Button Two"/>
           </div>
