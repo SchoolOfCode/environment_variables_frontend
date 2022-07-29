cd2b8d1eaa79176e7dfb2b55ba7abedbce43a778
Jenan <j.azzubaydi@gmail.com>
2022-07-29 11:37:05 +0100

    landing-banner fixed

--- project-app/components/Landing/Landing.jsx
+++ project-app/components/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const LandingBanner = () => {
   return ( 
     <div id="home" className='bg-hero-background min-h-screen hero-image bg-right-bottom bg-cover flex mt-[4rem]'>
 
-    <div class="relative container mx-auto p-4 flex items-center justify-center z-10" >
+    <div class="relative container mx-auto p-4 flex items-center justify-center z-10 h-48 w-2/4 mt-[10rem]" >
       <div > 
         <div class="flex flex-col justify-center items-center lg:items-start text-center">
           <h1 class="text-6xl font-semibold text-gray-900 leading-none align-center">Project Clean-Up</h1>
