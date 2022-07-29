8eb42cc59a793b194624d7439b72096d74e6dc00
Emma <e.e.gill@outlook.com>
2022-07-29 14:10:51 +0100

    Stats section completed, until back-end in place.

--- project-app/components/Stats/StatCard.jsx
+++ project-app/components/Stats/StatCard.jsx
@@ -2,11 +2,10 @@ import Image from "next/image";
 
 export default function StatCard({ image, number, text }) {
   return (
-    <div className="border-[0.1rem] b-[black] flex flex-col text-center w-48 h-48 pt-6">
-      <div className="p-4 border-[0.1rem] b-[blue]">
-        <Image src={image}>
-        {/* change p to a next Image tag and pass down url as image prop */}
-        <p>{number}</p> {/* number prop goes here */}
+    <div className="flex flex-col text-center w-48 h-48 shadow-lg">
+      <div className="p-2 text-[#FF9505] text-lg font-bold">
+        <Image src={image} width={75} height={75}/>
+        <p className="text-2xl m-0">{number}</p> {/* number prop goes here */}
         <p>{text}</p> {/* text prop goes here */}
       </div>
     </div>
--- project-app/components/Stats/Stats.jsx
+++ project-app/components/Stats/Stats.jsx
@@ -2,11 +2,11 @@ import StatCard from "./StatCard";
 
 export default function Stats() {
   return (
-    <div className="p-11 border-[0.1rem] border-[red]">
-      <div className="flex justify-around items-center space-x-5 border-[0.1rem] border-[green]">
-        <StatCard number="10" text="Cleanups Started" />
-        <StatCard number="10" text="Kilograms Collected" />
-        <StatCard number="10" text="No. of Volunteers" />
+    <div className="p-11">
+      <div className="flex justify-around items-center space-x-5">
+        <StatCard number="10" text="Cleanups Started" image="/icon-abacus.svg"/>
+        <StatCard number="10" text="Kilograms Collected" image="/icon-weight.svg"/>
+        <StatCard number="10" text="No. of Volunteers" image="/icon-people.svg"/>
       </div>
     </div>
   );
