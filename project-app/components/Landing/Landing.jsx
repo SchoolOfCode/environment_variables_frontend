e2e399db98186b1b20da08dcb4ca7ac3b67facd1
amiragucher <amiragucher@gmail.com>
2022-08-01 21:39:03 +0100

    Buttons are now in the center of the page

--- project-app/components/Landing/Landing.jsx
+++ project-app/components/Landing/Landing.jsx
@@ -4,15 +4,14 @@ const LandingBanner = () => {
   return (
     <div
       id="home"
-      className="bg-hero-background min-h-screen hero-image bg-right-bottom bg-cover flex mt-[4rem]"
-    >
+      className="bg-hero-background min-h-screen hero-image bg-right-bottom bg-cover flex mt-[4rem]">
       <div className="relative container mx-auto p-4 flex items-center justify-center z-10 h-48 w-2/4 mt-[10rem]">
         <div>
-          <div className="flex flex-col justify-center items-center text-center w-[50em] bg-[red]" >
-            <h1 className="flex justify-center text-6xl text-[#004F54] font-semibold text-gray-900 leading-none align-center mt-[5em] ">
+          <div className="flex flex-col justify-center items-center text-center w-[50em]">
+            <h1 className="text-[#004F54]-800 flex justify-center text-[70px] font-semibold leading-none align-center mt-[5em] ">
               Project Clean-Up
             </h1>
-            <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-row items-center justify-center space-x-[2em]">
               <NavButton buttonText="Join A Cleanup" className="flex items-center justify-center ml-[5em]"/>
               <NavButton buttonText="Start A Cleanup" className="flex items-center justify-center mr-[5em] "/>
             </div>
