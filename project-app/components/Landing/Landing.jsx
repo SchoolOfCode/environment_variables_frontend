5347e496298b08e2e16608e7a7281f491bf97485
amiragucher <amiragucher@gmail.com>
2022-08-01 21:23:49 +0100

    branch created, positioning buttons/making them bigger

--- project-app/components/Landing/Landing.jsx
+++ project-app/components/Landing/Landing.jsx
@@ -8,13 +8,13 @@ const LandingBanner = () => {
     >
       <div className="relative container mx-auto p-4 flex items-center justify-center z-10 h-48 w-2/4 mt-[10rem]">
         <div>
-          <div className="flex flex-col justify-center items-center lg:items-start text-center">
-            <h1 className="text-6xl font-semibold text-gray-900 leading-none align-center mt-[5em]">
+          <div className="flex flex-col justify-center items-center text-center w-[50em] bg-[red]" >
+            <h1 className="flex justify-center text-6xl text-[#004F54] font-semibold text-gray-900 leading-none align-center mt-[5em] ">
               Project Clean-Up
             </h1>
-            <div className="flex flex-row items-center justify-between space-x-8 md:ml-24">
-              <NavButton buttonText="Button One" />
-              <NavButton buttonText="Button Two" />
+            <div className="flex flex-row items-center justify-center">
+              <NavButton buttonText="Join A Cleanup" className="flex items-center justify-center ml-[5em]"/>
+              <NavButton buttonText="Start A Cleanup" className="flex items-center justify-center mr-[5em] "/>
             </div>
           </div>
         </div>
