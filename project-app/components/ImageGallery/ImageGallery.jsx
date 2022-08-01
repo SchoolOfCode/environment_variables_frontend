ad83389dfc08910cf9abed239fac9db8fb9da84d
edgarasmarkunas <markunas11@gmail.com>
2022-08-01 12:24:36 +0100

    carousel component functioning, just needs a little bit more styling

--- project-app/components/ImageGallery/ImageGallery.jsx
+++ project-app/components/ImageGallery/ImageGallery.jsx
@@ -1,28 +1,24 @@
 import { Carousel } from 'antd';
 import React from 'react';
-const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
+import Style from './ImageGallery.module.css'
 
 const ImageGallery = () => (
-  <Carousel autoplay>
-    <div>
-      <img style={contentStyle} src="https://i2-prod.manchestereveningnews.co.uk/incoming/article591425.ece/ALTERNATES/s810/C_71_article_1314725_image_list_image_list_item_0_image.jpg"/>
-    </div>
-    <div>
-      <img style={contentStyle} src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
-    </div>
-    <div>
-    <img style={contentStyle} src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
-    </div>
-    <div>
-    <img style={contentStyle} src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
-    </div>
+<div className={Style.imageContainer}>
+  <Carousel className={Style.image} autoplay>
+        <div >
+            <img  src="https://i2-prod.manchestereveningnews.co.uk/incoming/article591425.ece/ALTERNATES/s810/C_71_article_1314725_image_list_image_list_item_0_image.jpg"/>
+        </div>
+        <div >
+            <img  src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
+        </div>
+        <div >
+            <img  src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
+        </div>
+        <div >
+            <img  src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
+        </div>
   </Carousel>
+  </div>
 );
 
 export default ImageGallery;
