36df6e1c522b1a84c2d1cdc53d124598d21d9841
amiragucher <amiragucher@gmail.com>
2022-08-01 14:14:16 +0100

    Buttons changed to white, background created for the images

--- project-app/components/ImageGallery/ImageGallery.jsx
+++ project-app/components/ImageGallery/ImageGallery.jsx
@@ -4,18 +4,18 @@ import Style from './ImageGallery.module.css'
 
 const ImageGallery = () => (
 <div className={Style.imageContainer}>
-  <Carousel className={Style.image} autoplay>
+  <Carousel className={Style.Carousel} autoplay>
         <div >
-            <img  src="https://i2-prod.manchestereveningnews.co.uk/incoming/article591425.ece/ALTERNATES/s810/C_71_article_1314725_image_list_image_list_item_0_image.jpg"/>
+        <img className={Style.imageCSS} src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
         </div>
         <div >
-            <img  src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
+            <img className={Style.imageCSS} src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
         </div>
         <div >
-            <img  src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
+            <img className={Style.imageCSS} src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
         </div>
         <div >
-            <img  src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
+            <img className={Style.imageCSS} src="https://www.belvederecl.com/wp-content/uploads/2019/05/Community-cleanup-picture.jpg"/>
         </div>
   </Carousel>
   </div>
