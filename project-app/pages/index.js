1fc32d25c35804ba1f5c6e5e39cef8c580c4234c
amiragucher <amiragucher@gmail.com>
2022-08-01 15:59:00 +0100

    finished our mission

--- project-app/pages/index.js
+++ project-app/pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar/Navbar";
 import dynamic from "next/dynamic";
 import LandingBanner from "../components/Landing/Landing";
