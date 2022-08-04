7f2321a17a1b949fc267cf8f818e45c49f61fe3b
Emma <e.e.gill@outlook.com>
2022-08-04 18:39:04 +0100

    Added loginlogout to NavbarHome

--- project-app/components/Navbar/NavbarHome.jsx
+++ project-app/components/Navbar/NavbarHome.jsx
@@ -77,7 +77,13 @@ const NavbarHome = () => {
     />
   );
 
-  return (
+
+  //Auth0 check to see if user is logged in  
+  const { user, error, isLoading } = useUser();  
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+  if(user) { 
+    return (
     <div className="fixed z-[1002]">
       <div className=" flex justify-between w-[100vw] bg-[#004F54] text-[white] ml-[0px] px-[2em] py-[2em]">
         <div className="underline rounded-sm p-[5px] bg-[#006C72] text-[#white] text-[20px]">
@@ -104,9 +110,9 @@ const NavbarHome = () => {
             Weather
           </a>
         </Link>
-        <Link href="#contact">
+        <Link href="/api/auth/logout">
           <a className="text-[white] hidden md:flex items-center text-xl mr-[3em]">
-            Contact
+            {user.nickname} (Logout)
           </a>
         </Link>
         <AiOutlineMenu
@@ -183,9 +189,127 @@ const NavbarHome = () => {
               <a
                 onClick={() => setVisible(false)}
                 className="text-[#004F54] text-xl"
-                href="#contact"
+                href="/api/auth/logout"
               >
-                Contact
+                {user.nickname} (Logout)
+              </a>
+            </div>
+          </ul>
+        </Drawer>
+      </>
+    </div>
+  )};
+    
+  //Return this if the user is NOT logged in 
+  return (
+    <div className="fixed z-[1002]">
+      <div className=" flex justify-between w-[100vw] bg-[#004F54] text-[white] ml-[0px] px-[2em] py-[2em]">
+        <div className="underline rounded-sm p-[5px] bg-[#006C72] text-[#white] text-[20px]">
+          Project Clean-Up
+        </div>
+        <Link href="#home">
+          <a className="text-[white] hidden md:flex items-center text-xl">
+            Home
+          </a>
+        </Link>
+        <Dropdown
+          overlay={menu}
+          className="text-[white] hidden md:flex items-center text-xl"
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              Do Your Part
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>
+        <Link href="#weather">
+          <a className="text-[white] hidden md:flex items-center text-xl">
+            Weather
+          </a>
+        </Link>
+        <Link href="/api/auth/login">
+          <a className="text-[white] hidden md:flex items-center text-xl mr-[3em]">
+            Login
+          </a>
+        </Link>
+        <AiOutlineMenu
+          className="md:hidden absolute inset-y-[2em] right-[2em]"
+          size={25}
+          onClick={showDrawer}
+        />
+      </div>
+      <>
+        <Drawer
+          title="Information"
+          placement="right"
+          onClose={onClose}
+          visible={visible}
+          className="text-[#004F54]"
+        >
+          <ul className="flex flex-col ">
+            <div className="p-4 pt-10">
+              <a
+                onClick={() => setVisible(false)}
+                className="text-[#004F54] text-xl"
+                href="#home"
+              >
+                Home
+              </a>
+            </div>
+            <div className="p-4 ">
+              <a
+                onClick={() => setVisible(false)}
+                className="text-[#004F54] text-xl"
+                href="/#about"
+              >
+                About
+              </a>
+            </div>
+            <div className="p-4">
+              <a
+                onClick={() => setVisible(false)}
+                className="text-[#004F54] text-xl"
+                rel="noopener noreferrer"
+                href="/start-a-clean"
+              >
+                Start Cleanup
+              </a>
+            </div>
+            <div className="p-4">
+              <a
+                onClick={() => setVisible(false)}
+                className="text-[#004F54] text-xl"
+                href="#map"
+              >
+                Join Cleanup
+              </a>
+            </div>
+            <div className="p-4">
+              <a
+                onClick={() => setVisible(false)}
+                className="text-[#004F54] text-xl"
+                href="/log-a-clean"
+              >
+                Log Cleanup
+              </a>
+            </div>
+            <div className="p-4">
+              <a
+                onClick={() => setVisible(false)}
+                className="text-[#004F54] text-xl"
+                href="#weather"
+              >
+                Weather
+              </a>
+            </div>
+            <div className="p-4">
+              <a
+                onClick={() => setVisible(false)}
+                className="text-[#004F54] text-xl"
+                href="/api/auth/login"
+              >
+                Login
               </a>
             </div>
           </ul>
@@ -196,13 +320,16 @@ const NavbarHome = () => {
 };
 
 export default NavbarHome;
-//SideBar Menu Plan
 
-// Make seperate div that shows up when screen size is half(md)
-// In that div we need:
-// Close icon
-// Links
+/*
+SideBar Menu Plan
 
-// needs to have green bg
-// needs to be as tall as the screen
-// needs to open and close when hamburger icon/ x icon is clicked
+Make seperate div that shows up when screen size is half(md)
+In that div we need:
+  - Close icon
+  - Links
+
+- needs to have green bg
+- needs to be as tall as the screen
+- needs to open and close when hamburger icon/ x icon is clicked
+*/
