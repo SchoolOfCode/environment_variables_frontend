10a7674780f6f88a3ca795e643d8fbb2a650ffeb
Emma <e.e.gill@outlook.com>
2022-08-04 17:43:37 +0100

    Lost the wrapper on the _app somewhere in the merging, added it back in.

--- project-app/pages/_app.js
+++ project-app/pages/_app.js
@@ -1,3 +1,7 @@
+//Importing packages
+import { UserProvider } from '@auth0/nextjs-auth0';
+
+//Importing styles & fonts
 import "../styles/globals.css";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/500.css";
@@ -7,7 +11,11 @@ import "@fontsource/montserrat/800.css";
 import "@fontsource/montserrat/900.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp;
