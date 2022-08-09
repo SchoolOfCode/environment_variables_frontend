d52797582cba0919a399dd1425318ef2e30f7660
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-08-09 06:53:11 +0100

    changed main buttons y-spacing for mobiles, changed stats text size for mobiles

--- project-app/components/Landing/Landing.jsx
+++ project-app/components/Landing/Landing.jsx
@@ -1,7 +1,6 @@
 import NavButton from "./NavButton/NavButton.jsx";
 import Link from "next/link";
 
-
 const LandingBanner = () => {
   return (
     <div
@@ -14,14 +13,13 @@ const LandingBanner = () => {
             <h1 className="text-[40px] sm:text-[75px] text-[#004F54] flex justify-center font-semibold leading-none align-center mt-[3em] mb-[1em] mt-[4em]">
               Project Clean-Up
             </h1>
-            <div className="flex flex-col md:flex-row items-center justify-center md:space-x-[2em] space-y-[3em] md:space-y-[0]">
+            <div className="flex flex-col md:flex-row items-center justify-center md:space-x-[2em] space-y-[2em] md:space-y-[0]">
               <Link href="#map">
                 <a>
                   <NavButton
                     buttonText="Join Cleanup"
                     className="flex items-center justify-center"
                   />
-
                 </a>
               </Link>
               <Link rel="noopener noreferrer" href="/start-a-clean">
--- project-app/components/Stats/StatCard.jsx
+++ project-app/components/Stats/StatCard.jsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 export default function StatCard({ image, number, text }) {
   return (
     <div className="flex flex-col rounded-2xl text-center w-48 h-48 shadow-lg bg-white">
-      <div className="p-2 text-[#FF9505]  text-lg font-bold">
+      <div className="p-2 text-[#FF9505]  text-md sm:text-lg font-bold">
         <Image src={image} width={75} height={75} />
-        <p className="text-3xl m-0">{number}</p> {/* number prop goes here */}
+        <p className="text-2xl sm:text-3xl m-0">{number}</p>{" "}
+        {/* number prop goes here */}
         <p>{text}</p> {/* text prop goes here */}
       </div>
     </div>
