0f35d599b4c9a36b717fb9ebcb98c6022c553347
edgarasmarkunas <markunas11@gmail.com>
2022-08-10 15:26:02 +0100

    added a test that checks for our test section

--- project-app/cypress/e2e/projectCleanup.cy.js
+++ project-app/cypress/e2e/projectCleanup.cy.js
@@ -2,15 +2,20 @@ describe('loading up our page', () => {
     it('brings us to home page of app and brings to map', () => {
       cy.visit('https://projectcleanup.netlify.app/')
 
+      cy.contains('Cleanups Started')
+      cy.contains('Kilograms Collected')
+      cy.contains('No. of Volunteers')
+
       cy.contains('Join Cleanup').click()
 
       cy.url().should('include', '#map')
 
-      cy.get('[href="/#weather"]').click()
+        //Uncomment this when the navbar is back
+      // cy.get('[href="/#weather"]').click()
 
       cy.get('#default-search').clear().type('dubai').get('.relative > .text-white').click()
 
-      cy.contains('Tips').click().get('.justify-end > .text-\[\#004F54\]').click()
+      cy.contains('Tips').click()
 
       //This won't work until we add cypress as a user in auth0 settings
     //   cy.contains('Start Cleanup').click()
