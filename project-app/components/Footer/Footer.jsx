d63ec8f01dcedd74c584f67591cd543c85a16c15
Emma <e.e.gill@outlook.com>
2022-08-10 10:56:48 +0100

    Added app logo to footer.

--- project-app/components/Footer/Footer.jsx
+++ project-app/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Image from "next/image";
 import {
   AiFillFacebook,
   AiFillInstagram,
@@ -9,13 +9,17 @@ export default function Footer() {
   return (
     <footer className="flex justify-between items-center w-full h-[6rem] px-1 sm:px-4 bg-[#004F54]">
       {/* Logo */}
-      <div className="text-white w-1/4 sm:w-56">LOGO</div>
+      <div className="flex flex-row h-auto w-1/4 sm:w-56">
+      <div className="h-12 w-12 sm:h-16 sm:w-16 relative">
+        <Image src="/logo-app.png" alt="Project Clean-up logo" layout="fill" objectFit="cover"/>
+      </div>
+      </div>
       {/* Built by and copyright */}
       <div className="mt-3 sm:mt-6 text-center">
-        <span className="text-white text-sm sm:text-base">
+        <span className="text-white text-xs sm:text-sm">
           Built by the Environment Variables
         </span>
-        <p className="text-xs sm:text-sm text-gray-50">
+        <p className="text-[.5rem] sm:text-xs text-gray-50">
           Copyright &copy;2022
         </p>
       </div>
