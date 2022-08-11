8bcd686febd62466ea64290208ec4818f577d73d
Jenan <j.azzubaydi@gmail.com>
2022-08-11 14:04:54 +0100

    uncomented codes and removes lines 19-20

--- project-app/cypress/e2e/projectCleanup.cy.js
+++ project-app/cypress/e2e/projectCleanup.cy.js
@@ -1,26 +1,23 @@
-// describe('loading up our page', () => {
-//     it('brings us to home page of app and brings to map', () => {
-//       cy.visit('https://projectcleanup.netlify.app/')
+describe('loading up our page', () => {
+    it('brings us to home page of app and brings to map', () => {
+      cy.visit('https://projectcleanup.netlify.app/')
 
-//       cy.contains('Cleanups Started')
-//       cy.contains('Kilograms Collected')
-//       cy.contains('No. of Volunteers')
+      cy.contains('Cleanups Started')
+      cy.contains('Kilograms Collected')
+      cy.contains('No. of Volunteers')
 
-//       cy.contains('Join A Clean').click()
+      cy.contains('Join A Clean').click()
 
-//       cy.url().should('include', '#map')
+      cy.url().should('include', '#map')
 
-//       cy.get('[href="/#weather"]').click()
+      cy.get('[href="/#weather"]').click()
 
-//       cy.get('#default-search').clear().type('dubai').get('.relative > .text-white').click()
+      cy.get('#default-search').clear().type('dubai').get('.relative > .text-white').click()
 
-//       cy.contains('Tips').click()
+      //cy.contains('Tips').click()
 
-      //This won't work until we add cypress as a user in auth0 settings
-    //   cy.contains('Start Cleanup').click()
-
-  //   })
-  // })
+    })
+  })
 
   describe('Accessing start clean form', () => {
     it("Logs in with Auth0", () => {
