150e3b890aaa86e38cab5948cc1d68dd58229480
edgarasmarkunas <markunas11@gmail.com>
2022-08-11 13:45:48 +0100

    Cypress now is able to log in through auth0, and added some simple tests on the start clean form

--- project-app/cypress/e2e/projectCleanup.cy.js
+++ project-app/cypress/e2e/projectCleanup.cy.js
@@ -1,24 +1,41 @@
-describe('loading up our page', () => {
-    it('brings us to home page of app and brings to map', () => {
-      cy.visit('https://projectcleanup.netlify.app/')
+// describe('loading up our page', () => {
+//     it('brings us to home page of app and brings to map', () => {
+//       cy.visit('https://projectcleanup.netlify.app/')
 
-      cy.contains('Cleanups Started')
-      cy.contains('Kilograms Collected')
-      cy.contains('No. of Volunteers')
+//       cy.contains('Cleanups Started')
+//       cy.contains('Kilograms Collected')
+//       cy.contains('No. of Volunteers')
 
-      cy.contains('Join Cleanup').click()
+//       cy.contains('Join A Clean').click()
 
-      cy.url().should('include', '#map')
+//       cy.url().should('include', '#map')
 
-        //Uncomment this when the navbar is back
-      // cy.get('[href="/#weather"]').click()
+//       cy.get('[href="/#weather"]').click()
 
-      cy.get('#default-search').clear().type('dubai').get('.relative > .text-white').click()
+//       cy.get('#default-search').clear().type('dubai').get('.relative > .text-white').click()
 
-      cy.contains('Tips').click()
+//       cy.contains('Tips').click()
 
       //This won't work until we add cypress as a user in auth0 settings
     //   cy.contains('Start Cleanup').click()
 
+  //   })
+  // })
+
+  describe('Accessing start clean form', () => {
+    it("Logs in with Auth0", () => {
+      cy.visit("https://projectcleanup.netlify.app/");
+      cy.contains('Start A Clean').click()
+
+      cy.origin("https://dev-659k2jkc.us.auth0.com/", () => {
+        cy.get("#username").type(Cypress.env("USERNAME"));
+        cy.get("#password").type(Cypress.env("PASSWORD"));
+        cy.contains("Continue").click()
+        })
+
+        cy.url().should('include', 'start-a-clean')
+        cy.get('[name="cleanName"]').type('London Clean')
+        cy.get('[href="/"]').click()
+        cy.url().should('include', 'https://projectcleanup.netlify.app/')
     })
   })
