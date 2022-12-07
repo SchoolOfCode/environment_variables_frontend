54dc56e0371a1a58387293d69942acb2d908777c
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-12-07 07:39:49 +0000

    addde comment

--- project-app/pages/log-a-clean.js
+++ project-app/pages/log-a-clean.js
@@ -2,7 +2,7 @@
 
 //Importing packages
 import Head from "next/head";
-import {withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 //Importing custome components
 import LogCleanForm from "../components/Forms/LogClean";
@@ -12,27 +12,30 @@ import Footer from "../components/Footer/Footer";
 export default function LogClean() {
   return (
     <div className="flex flex-col h-screen">
-      
       <Head>
         <title>Project Clean-Up | Log a Clean</title>
       </Head>
       <NavbarLogClean />
-     
-      <h3 className="pt-32  text-5xl text-center text-[#004F54]">Log a Clean</h3>
+
+      <h3 className="pt-32  text-5xl text-center text-[#004F54]">
+        Log a Clean
+      </h3>
       <div className="flex flex-col h-full justify-center">
-      <div className="flex justify-center  ">
-      <h3 className="  mb-0 text-sm sm:text-lg rounded-md text-[#ffffff] bg-[#FF9505] w-11/12 sm:w-[34rem] p-1 text-center">
-          Log your clean, then our stats will update with your contribution!
-        </h3>
+        <div className="flex justify-center  ">
+          <h3 className="  mb-0 text-sm sm:text-lg rounded-md text-[#ffffff] bg-[#FF9505] w-11/12 sm:w-[34rem] p-1 text-center">
+            Log your clean, then our stats will update with your contribution!
+          </h3>
         </div>
 
         <div className="flex justify-center mb-14 pt-4">
           <LogCleanForm />
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
 
-export const getServerSideProps = withPageAuthRequired(); 
+export const getServerSideProps = withPageAuthRequired();
+
+// added comment to force push
